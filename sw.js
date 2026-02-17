4f32b667d11da5a556c2ac1891ddbe8bef7a35d0
Wilmar007 <wilmarmoreno07@gmail.com>
2026-02-17 20:08:19 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "electricity-lab-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./circuit-builder.html",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch (serve cached files when offline)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
